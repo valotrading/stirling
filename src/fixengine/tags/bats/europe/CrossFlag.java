f48dccc079bc0186c277902e26549bdfbcc20e34
Pekka Enberg <penberg@reaktor.fi>
2010-08-26 14:47:02 +0300

    fixengine: Fix bats europe class package names
    Signed-off-by: Pekka Enberg <penberg@reaktor.fi>

--- src/fixengine/tags/bats/europe/CrossFlag.java
+++ src/fixengine/tags/bats/europe/CrossFlag.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fixengine.tags;
+package fixengine.tags.bats.europe;
 
 import fixengine.messages.bats.europe.CrossFlagValue;
 import fixengine.messages.EnumField;
