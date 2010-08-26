0d76d0d8add1556daf38a878848c7ad2fa8bd0eb
Pekka Enberg <penberg@kernel.org>
2010-08-26 21:45:55 +0300

    fixengine: Fix compilation error
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/tags/OrderCapacity.java
+++ src/fixengine/tags/OrderCapacity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fixengine.tags.bats.europe;
+package fixengine.tags;
 
 import fixengine.messages.OrderCapacityValue;
 
