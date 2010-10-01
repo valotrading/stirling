d712e80565eb1402071c85a764856aa2d9ded10b
Karim Osman <karim.osman@reaktor.fi>
2010-10-01 12:44:17 +0300

    fixengine: Fix method ordering in Session
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <pekka.enberg@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@reaktor.fi>

--- src/fixengine/session/Session.java
+++ src/fixengine/session/Session.java
@@ -101,45 +101,6 @@ public class Session {
         store.load(this);
     }
 
-    public Config getConfig() {
-        return config;
-    }
-
-    public Sequence getOutgoingSeq() {
-        return outgoingSeq;
-    }
-
-    public void setOutgoingSeq(Sequence seq) {
-        outgoingSeq = seq;
-    }
-
-    public Sequence getIncomingSeq() {
-        Sequence seq = new Sequence();
-        seq.reset(queue.nextSeqNum());
-        return seq;
-    }
-
-    public void setIncomingSeq(Sequence seq) {
-        queue.reset(seq.peek());
-    }
-
-    public DateTime currentTime() {
-        return timeSource.currentTime();
-    }
-
-    public boolean isAuthenticated() {
-        return authenticated;
-    }
-
-    public void send(Connection conn, Message message) {
-        message.setHeaderConfig(config);
-        message.setMsgSeqNum(outgoingSeq.next());
-        message.setSendingTime(currentTime());
-        conn.send(silvertip.Message.fromString(message.format()));
-        prevTxTime = currentTime();
-        store.save(this);
-    }
-
     public void receive(final Connection conn, silvertip.Message message, final MessageVisitor visitor) {
         prevRxTime = currentTime();
         try {
@@ -195,89 +156,36 @@ public class Session {
         }
     }
 
-    public void logon(Connection conn) {
-        authenticated = initiatedLogout = false;
-        LogonMessage message = (LogonMessage) messageFactory.create(LOGON);
-        message.setInteger(HeartBtInt.TAG, 30);
-        message.setEnum(EncryptMethod.TAG, EncryptMethodValue.NONE);
-        send(conn, message);
-    }
+    private boolean validate(final Connection conn, final Message message) {
+        return MessageValidator.validate(this, message, new ErrorHandler() {
+            @Override public void sessionReject(SessionRejectReasonValue reason, String text, ErrorLevel level, boolean terminate) {
+                logError(text, level);
+                Session.this.sessionReject(conn, message, reason, text);
+                if (terminate)
+                    Session.this.terminate(conn, message, text);
+            }
 
-    public void logout(final Connection conn) {
-        send(conn, (LogoutMessage) messageFactory.create(LOGOUT));
-        initiatedLogout = true;
-        logoutInitiatedAt = currentTime();
-        waitingForResponseToInitiatedLogout = true;
-    }
+            @Override public void businessReject(BusinessRejectReasonValue reason, String text, ErrorLevel level) {
+                logError(text, level);
+                Session.this.businessReject(conn, message.getMsgType(), message.getMsgSeqNum(), reason, text);
+            }
 
-    public void sequenceReset(Connection conn, Sequence seq) {
-        SequenceResetMessage message = (SequenceResetMessage) messageFactory.create(SEQUENCE_RESET);
-        message.setHeaderConfig(config);
-        message.setSendingTime(currentTime());
-        message.setMsgSeqNum(seq.peek());
-        message.setInteger(NewSeqNo.TAG, seq.next());
-        message.setBoolean(GapFillFlag.TAG, false);
-        conn.send(silvertip.Message.fromString(message.format()));
-        prevTxTime = currentTime();
-        setOutgoingSeq(seq);
-        store.save(this);
-    }
+            @Override public void terminate(String text) {
+                logError(text, ErrorLevel.ERROR);
+                Session.this.terminate(conn, message, text);
+            }
 
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void keepAlive(Connection conn) {
-        if (isTimedOut(prevTxTime, heartBtInt.heartbeat().delayMsec())) {
-            heartbeat(conn);
-            prevTxTime = currentTime();
-        }
-
-        if (isTimedOut(prevRxTime, heartBtInt.testRequest().delayMsec())) {
-            testRequest(conn);
-            prevRxTime = currentTime();
-        }
-    }
-
-    public void processInitiatedLogout(Connection conn) {
-        if (waitingForResponseToInitiatedLogout && isTimedOut(logoutInitiatedAt, getLogoutResponseTimeoutMsec())) {
-            getLogger().warning("Response to logout not received in " + getLogoutResponseTimeoutMsec() / 1000
-                    + " second(s), disconnecting");
-            waitingForResponseToInitiatedLogout = false;
-            conn.close();
-        }
-    }
-
-    private boolean isTimedOut(DateTime dateTime, long timeoutMsec) {
-        DateTime now = currentTime();
-        DateTime timeOutAt = dateTime.plusMillis((int)timeoutMsec);
-        return now.isAfter(timeOutAt);
-    }
-
-    public void heartbeat(Connection conn) {
-        send(conn, (HeartbeatMessage) messageFactory.create(HEARTBEAT));
-    }
-
-    protected long getLogoutResponseTimeoutMsec() {
-        return DEFAULT_LOGOUT_RESPONSE_TIMEOUT_MSEC;
-    }
-
-    protected Logger getLogger() {
-        return LOG;
-    }
-
-    protected boolean checkSeqResetSeqNum() {
-        return true;
-    }
-
-    private void testRequest(Connection conn) {
-        TestRequestMessage req = (TestRequestMessage) messageFactory.create(TEST_REQUEST);
-        req.setString(TestReqID.TAG, Long.toString(++testReqId));
-        send(conn, req);
+            private void logError(String text, ErrorLevel level) {
+                switch (level) {
+                case WARNING:
+                    getLogger().warning(text);
+                    break;
+                case ERROR:
+                    getLogger().severe(text);
+                    break;
+                }
+            }
+        });
     }
 
     private void process(final Connection conn, Message message, final MessageVisitor visitor) {
@@ -337,40 +245,47 @@ public class Session {
         }
     }
 
-    private boolean validate(final Connection conn, final Message message) {
-        return MessageValidator.validate(this, message, new ErrorHandler() {
-            @Override public void sessionReject(SessionRejectReasonValue reason, String text, ErrorLevel level, boolean terminate) {
-                logError(text, level);
-                Session.this.sessionReject(conn, message, reason, text);
-                if (terminate)
-                    Session.this.terminate(conn, message, text);
-            }
-
-            @Override public void businessReject(BusinessRejectReasonValue reason, String text, ErrorLevel level) {
-                logError(text, level);
-                Session.this.businessReject(conn, message.getMsgType(), message.getMsgSeqNum(), reason, text);
-            }
-
-            @Override public void terminate(String text) {
-                logError(text, ErrorLevel.ERROR);
-                Session.this.terminate(conn, message, text);
-            }
-
-            private void logError(String text, ErrorLevel level) {
-                switch (level) {
-                case WARNING:
-                    getLogger().warning(text);
-                    break;
-                case ERROR:
-                    getLogger().severe(text);
-                    break;
-                }
-            }
-        });
-    }
-
     private boolean isOutOfSync() {
         return queue.hasSeqNumGap();
+    }
+
+    private void syncMessages(Connection conn) {
+        int beginSeqNo = queue.nextSeqNum();
+        ResendRequestMessage resendReq = (ResendRequestMessage) messageFactory.create(RESEND_REQUEST);
+        resendReq.setInteger(BeginSeqNo.TAG, beginSeqNo);
+        resendReq.setInteger(EndSeqNo.TAG, 0);
+        send(conn, resendReq);
+    }
+
+    private void fillSequenceGap(Connection conn, int newSeqNo) {
+        SequenceResetMessage seqReset = (SequenceResetMessage) messageFactory.create(SEQUENCE_RESET);
+        seqReset.setPossDupFlag(true);
+        seqReset.setBoolean(GapFillFlag.TAG, true);
+        seqReset.setInteger(NewSeqNo.TAG, newSeqNo);
+        send(conn, seqReset);
+        outgoingSeq.reset(newSeqNo);
+    }
+
+    private void processSeqReset(Connection conn, SequenceResetMessage message) {
+        int newSeqNo = message.getInteger(NewSeqNo.TAG);
+        if (checkSeqResetSeqNum() && !message.isResetOk(queue.nextSeqNum())) {
+            int beginSeqNo = queue.nextSeqNum();
+            ResendRequestMessage resendReq = (ResendRequestMessage) messageFactory.create(RESEND_REQUEST);
+            resendReq.setInteger(BeginSeqNo.TAG, beginSeqNo);
+            resendReq.setInteger(EndSeqNo.TAG, message.getMsgSeqNum() - 1);
+            send(conn, resendReq);
+        } else if (newSeqNo <= message.getMsgSeqNum() && message.getBoolean(GapFillFlag.TAG)) {
+            sessionReject(conn, message.getMsgSeqNum(), SessionRejectReasonValue.INVALID_VALUE,
+                "Attempt to lower sequence number, invalid value NewSeqNum(36)=" + newSeqNo);
+        } else if (newSeqNo < message.getMsgSeqNum() && !message.getBoolean(GapFillFlag.TAG)) {
+            getLogger().warning("Value is incorrect (out of range) for this tag, NewSeqNum(36)=" + newSeqNo);
+            sessionReject(conn, message.getMsgSeqNum(), SessionRejectReasonValue.INVALID_VALUE,
+                "Value is incorrect (out of range) for this tag, NewSeqNum(36)=" + newSeqNo);
+        } else {
+            if (newSeqNo == message.getMsgSeqNum() && !message.getBoolean(GapFillFlag.TAG))
+                getLogger().warning("NewSeqNo(36)=" + newSeqNo + " is equal to expected MsgSeqNum(34)=" + message.getMsgSeqNum());
+            queue.reset(newSeqNo);
+        }
     }
 
     private void sessionReject(Connection conn, Message message, SessionRejectReasonValue reason, String text) {
@@ -401,42 +316,127 @@ public class Session {
         conn.close();
     }
 
-    private void fillSequenceGap(Connection conn, int newSeqNo) {
-        SequenceResetMessage seqReset = (SequenceResetMessage) messageFactory.create(SEQUENCE_RESET);
-        seqReset.setPossDupFlag(true);
-        seqReset.setBoolean(GapFillFlag.TAG, true);
-        seqReset.setInteger(NewSeqNo.TAG, newSeqNo);
-        send(conn, seqReset);
-        outgoingSeq.reset(newSeqNo);
+    public void logon(Connection conn) {
+        authenticated = initiatedLogout = false;
+        LogonMessage message = (LogonMessage) messageFactory.create(LOGON);
+        message.setInteger(HeartBtInt.TAG, 30);
+        message.setEnum(EncryptMethod.TAG, EncryptMethodValue.NONE);
+        send(conn, message);
     }
 
-    private void syncMessages(Connection conn) {
-        int beginSeqNo = queue.nextSeqNum();
-        ResendRequestMessage resendReq = (ResendRequestMessage) messageFactory.create(RESEND_REQUEST);
-        resendReq.setInteger(BeginSeqNo.TAG, beginSeqNo);
-        resendReq.setInteger(EndSeqNo.TAG, 0);
-        send(conn, resendReq);
+    public void logout(final Connection conn) {
+        send(conn, (LogoutMessage) messageFactory.create(LOGOUT));
+        initiatedLogout = true;
+        logoutInitiatedAt = currentTime();
+        waitingForResponseToInitiatedLogout = true;
     }
 
-    private void processSeqReset(Connection conn, SequenceResetMessage message) {
-        int newSeqNo = message.getInteger(NewSeqNo.TAG);
-        if (checkSeqResetSeqNum() && !message.isResetOk(queue.nextSeqNum())) {
-            int beginSeqNo = queue.nextSeqNum();
-            ResendRequestMessage resendReq = (ResendRequestMessage) messageFactory.create(RESEND_REQUEST);
-            resendReq.setInteger(BeginSeqNo.TAG, beginSeqNo);
-            resendReq.setInteger(EndSeqNo.TAG, message.getMsgSeqNum() - 1);
-            send(conn, resendReq);
-        } else if (newSeqNo <= message.getMsgSeqNum() && message.getBoolean(GapFillFlag.TAG)) {
-            sessionReject(conn, message.getMsgSeqNum(), SessionRejectReasonValue.INVALID_VALUE,
-                "Attempt to lower sequence number, invalid value NewSeqNum(36)=" + newSeqNo);
-        } else if (newSeqNo < message.getMsgSeqNum() && !message.getBoolean(GapFillFlag.TAG)) {
-            getLogger().warning("Value is incorrect (out of range) for this tag, NewSeqNum(36)=" + newSeqNo);
-            sessionReject(conn, message.getMsgSeqNum(), SessionRejectReasonValue.INVALID_VALUE,
-                "Value is incorrect (out of range) for this tag, NewSeqNum(36)=" + newSeqNo);
-        } else {
-            if (newSeqNo == message.getMsgSeqNum() && !message.getBoolean(GapFillFlag.TAG))
-                getLogger().warning("NewSeqNo(36)=" + newSeqNo + " is equal to expected MsgSeqNum(34)=" + message.getMsgSeqNum());
-            queue.reset(newSeqNo);
+    public void sequenceReset(Connection conn, Sequence seq) {
+        SequenceResetMessage message = (SequenceResetMessage) messageFactory.create(SEQUENCE_RESET);
+        message.setHeaderConfig(config);
+        message.setSendingTime(currentTime());
+        message.setMsgSeqNum(seq.peek());
+        message.setInteger(NewSeqNo.TAG, seq.next());
+        message.setBoolean(GapFillFlag.TAG, false);
+        conn.send(silvertip.Message.fromString(message.format()));
+        prevTxTime = currentTime();
+        setOutgoingSeq(seq);
+        store.save(this);
+    }
+
+    public void keepAlive(Connection conn) {
+        if (isTimedOut(prevTxTime, heartBtInt.heartbeat().delayMsec())) {
+            heartbeat(conn);
+            prevTxTime = currentTime();
         }
+
+        if (isTimedOut(prevRxTime, heartBtInt.testRequest().delayMsec())) {
+            testRequest(conn);
+            prevRxTime = currentTime();
+        }
+    }
+
+    public void heartbeat(Connection conn) {
+        send(conn, (HeartbeatMessage) messageFactory.create(HEARTBEAT));
+    }
+
+    private void testRequest(Connection conn) {
+        TestRequestMessage req = (TestRequestMessage) messageFactory.create(TEST_REQUEST);
+        req.setString(TestReqID.TAG, Long.toString(++testReqId));
+        send(conn, req);
+    }
+
+    public void send(Connection conn, Message message) {
+        message.setHeaderConfig(config);
+        message.setMsgSeqNum(outgoingSeq.next());
+        message.setSendingTime(currentTime());
+        conn.send(silvertip.Message.fromString(message.format()));
+        prevTxTime = currentTime();
+        store.save(this);
+    }
+
+    public void processInitiatedLogout(Connection conn) {
+        if (waitingForResponseToInitiatedLogout && isTimedOut(logoutInitiatedAt, getLogoutResponseTimeoutMsec())) {
+            getLogger().warning("Response to logout not received in " + getLogoutResponseTimeoutMsec() / 1000
+                    + " second(s), disconnecting");
+            waitingForResponseToInitiatedLogout = false;
+            conn.close();
+        }
+    }
+
+    private boolean isTimedOut(DateTime dateTime, long timeoutMsec) {
+        DateTime now = currentTime();
+        DateTime timeOutAt = dateTime.plusMillis((int)timeoutMsec);
+        return now.isAfter(timeOutAt);
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public Sequence getOutgoingSeq() {
+        return outgoingSeq;
+    }
+
+    public void setOutgoingSeq(Sequence seq) {
+        outgoingSeq = seq;
+    }
+
+    public Sequence getIncomingSeq() {
+        Sequence seq = new Sequence();
+        seq.reset(queue.nextSeqNum());
+        return seq;
+    }
+
+    public void setIncomingSeq(Sequence seq) {
+        queue.reset(seq.peek());
+    }
+
+    public DateTime currentTime() {
+        return timeSource.currentTime();
+    }
+
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    protected long getLogoutResponseTimeoutMsec() {
+        return DEFAULT_LOGOUT_RESPONSE_TIMEOUT_MSEC;
+    }
+
+    protected Logger getLogger() {
+        return LOG;
+    }
+
+    protected boolean checkSeqResetSeqNum() {
+        return true;
     }
 }
