fd7cda482e14d4e3ad09c290d2eead1d6253f70e
Kari Husa <kari.husa@reaktor.fi>
2010-11-09 15:38:57 +0200

    fixengine: Add Hotspot FX specific Order Cancel Reject Message
    Signed-off-by: Kari Husa <kari.husa@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix42/hotspotfx/OrderCancelRejectMessage.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix42.hotspotfx;
+
+import fixengine.messages.MessageHeader;
+import fixengine.messages.Required;
+import fixengine.tags.Account;
+import fixengine.tags.ClOrdID;
+import fixengine.tags.CxlRejReason;
+import fixengine.tags.CxlRejResponseTo;
+import fixengine.tags.OrderID;
+import fixengine.tags.OrigClOrdID;
+import fixengine.tags.Symbol;
+import fixengine.tags.Text;
+
+public class OrderCancelRejectMessage extends fixengine.messages.fix42.OrderCancelRejectMessage {
+    public OrderCancelRejectMessage(MessageHeader header) {
+        super(header);
+    }
+
+    @Override public void fields() {
+        field(Account.TAG, Required.NO);
+        field(ClOrdID.TAG);
+        field(OrderID.TAG);
+        field(OrigClOrdID.TAG);
+        field(Symbol.TAG);
+        field(CxlRejResponseTo.TAG);
+        field(CxlRejReason.TAG, Required.NO);
+        field(Text.TAG, Required.NO);
+    }
+}
