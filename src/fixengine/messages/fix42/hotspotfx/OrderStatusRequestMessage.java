da710d7b06eaad8cabcd8f17cc7d7df7e785681f
Kari Husa <kari.husa@reaktor.fi>
2010-11-09 15:38:59 +0200

    fixengine: Add Hotspot FX specific Order Status Request message
    Signed-off-by: Kari Husa <kari.husa@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix42/hotspotfx/OrderStatusRequestMessage.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix42.hotspotfx;
+
+import fixengine.messages.MessageHeader;
+import fixengine.messages.Required;
+import fixengine.tags.Account;
+import fixengine.tags.ClOrdID;
+import fixengine.tags.Side;
+import fixengine.tags.Symbol;
+import fixengine.tags.TransactTime;
+
+public class OrderStatusRequestMessage extends fixengine.messages.fix42.OrderStatusRequestMessage {
+    public OrderStatusRequestMessage(MessageHeader header) {
+        super(header);
+    }
+
+    @Override protected void fields() {
+        field(Account.TAG, Required.NO);
+        field(ClOrdID.TAG);
+        field(Symbol.TAG, Required.NO);
+        field(Side.TAG, Required.NO);
+        field(TransactTime.TAG, Required.NO);
+    }
+}
