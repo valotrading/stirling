33be920e64623be1134b27f29f71055ad83cca1b
Karim Osman <karim.osman@reaktor.fi>
2010-11-12 14:46:40 +0200

    fixengine: Add Burgundy specific ExecInst(18)
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/burgundy/ExecInstValue.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44.burgundy;
+
+import fixengine.messages.Formattable;
+import fixengine.messages.InvalidValueForTagException;
+
+public enum ExecInstValue implements Formattable {
+    MIDPOINT('M'),
+    MARKET_PEG('P'),
+    PRIMARY_PEG('R'),
+    CANCEL_ON_SYS_FAIL('Q');
+
+    private char value;
+
+    ExecInstValue(char value) {
+        this.value = value;
+    }
+
+    @Override public String value() {
+        return Character.toString(value);
+    }
+
+    public static ExecInstValue parse(char value) {
+        for (ExecInstValue type : ExecInstValue.values()) {
+            if (type.value == value)
+                return type;
+        }
+        throw new InvalidValueForTagException(Character.toString(value));
+    }
+}
