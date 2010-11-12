274ce561323c9861a0b89c1e881f7bf3b32da50e
Karim Osman <karim.osman@reaktor.fi>
2010-11-12 14:46:35 +0200

    fixengine: Add PegMoveType(835)
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/PegMoveTypeValue.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44;
+
+import fixengine.messages.Formattable;
+import fixengine.messages.InvalidValueForTagException;
+
+public enum PegMoveTypeValue implements Formattable {
+    FLOATING('0'),
+    FIXED('1');
+
+    private char value;
+
+    PegMoveTypeValue(char value) {
+        this.value = value;
+    }
+
+    @Override public String value() {
+        return Character.toString(value);
+    }
+
+    public static PegMoveTypeValue parse(char value) {
+        for (PegMoveTypeValue type : PegMoveTypeValue.values()) {
+            if (type.value == value)
+                return type;
+        }
+        throw new InvalidValueForTagException(Character.toString(value));
+    }
+}
