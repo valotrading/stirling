e8c854dee3de0108aa55ca2727cf2c78e4583fe9
Karim Osman <karim.osman@reaktor.fi>
2010-11-22 12:06:57 +0200

    xtch: Add template for Turquoise Logon message
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

---
+++ src/xtch/turquoise/templates/Logon.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xtch.turquoise.templates;
+
+import java.nio.ByteBuffer;
+
+import xtch.templates.MessageTemplate;
+import xtch.turquoise.Fields;
+import xtch.turquoise.MessageType;
+import xtch.turquoise.messages.TurquoiseMessage;
+
+/** 
+ * Template for Logon message as specified in section 7.3.1 of [2].
+ */
+public class Logon extends MessageTemplate<TurquoiseMessage> {
+  public static final Logon TEMPLATE = new Logon();
+
+  public Logon() {
+    super(MessageType.LOGON);
+    add(Fields.COMP_ID);
+    add(Fields.PASSWORD);
+    add(Fields.NEW_PASSWORD);
+    add(Fields.MESSAGE_VERSION);
+  }
+
+  @Override public void encode(ByteBuffer buffer, TurquoiseMessage message) {
+    super.encode(buffer, message);
+    buffer.put((byte) '\n');
+  }
+
+  @Override protected TurquoiseMessage newFieldContainer() { 
+    return new TurquoiseMessage(this);
+  }
+}
