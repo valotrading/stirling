cd6b40161a25a3f0287522ab9cd4467a4e411382
Karim Osman <karim.osman@reaktor.fi>
2010-11-24 17:44:17 +0200

    xtch: Add Turquoise message header
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- src/xtch/turquoise/Fields.java
+++ src/xtch/turquoise/Fields.java
@@ -16,17 +16,28 @@
 package xtch.turquoise;
 
 import xtch.elements.Field;
+import xtch.turquoise.types.Alpha;
 import xtch.turquoise.types.AsciiString;
 import xtch.turquoise.types.SignedInt32;
 import xtch.turquoise.types.SignedInt8;
+import xtch.turquoise.types.UnsignedInt16;
 
 public class Fields {
+  public static final Field<Integer> START_OF_MESSAGE = new SignedInt8Field("StartOfMessage");
+  public static final Field<Integer> MESSAGE_LENGTH = new UnsignedInt16Field("MessageLength");
+  public static final Field<Character> MESSAGE_TYPE = new AlphaField("MessageType");
   public static final Field<String> COMP_ID = new StringField("CompID", 25);
   public static final Field<String> PASSWORD = new StringField("Password", 25);
   public static final Field<String> NEW_PASSWORD = new StringField("NewPassword", 25);
   public static final Field<Integer> MESSAGE_VERSION = new SignedInt8Field("SequenceNumber");
   public static final Field<Integer> REJECT_CODE = new SignedInt32Field("RejectCode");
   public static final Field<String> PASSWORD_EXPIRY_DAY_COUNT = new StringField("PasswordExpiryDayCount", 30);
+  
+  private static class AlphaField extends Field<Character> {
+    protected AlphaField(String name) {
+      super(name, Alpha.TYPE, 1);
+    }
+  }
 
   private static class StringField extends Field<String> {
     protected StringField(String name, int length) {
@@ -37,6 +48,12 @@ public class Fields {
   private static class SignedInt8Field extends Field<Integer> {
     protected SignedInt8Field(String name) {
       super(name, SignedInt8.TYPE, 1);
+    }
+  }
+
+  private static class UnsignedInt16Field extends Field<Integer> {
+    protected UnsignedInt16Field(String name) {
+      super(name, UnsignedInt16.TYPE, 2);
     }
   }
 
