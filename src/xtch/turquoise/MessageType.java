cccd4ffc233d9fac9272952cf89fb51553315ad0
Karim Osman <karim.osman@reaktor.fi>
2010-11-25 13:33:53 +0200

    xtch: Define Turquoise message types
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- src/xtch/turquoise/MessageType.java
+++ src/xtch/turquoise/MessageType.java
@@ -18,10 +18,32 @@ package xtch.turquoise;
 public class MessageType {
   public static final MessageType TYPE = new MessageType();
 
+  /**
+   * Administrative message types as specified in section 7.1.1 of [2].
+   */
   public static final String LOGON = "A";
   public static final String LOGON_REPLY = "B";
   public static final String LOGOUT = "5";
   public static final String HEARTBEAT = "0";
+  public static final String MISSED_MESSAGE_REQUEST = "M";
+  public static final String MISSED_MESSAGE_REQUEST_ACK = "N";
+  public static final String MISSED_MESSAGE_REPORT = "P";
+  public static final String REJECT = "3";
+
+  /**
+   * Client-initiated message types as specified in section 7.1.2.1 of [2].
+   */
+  public static final String NEW_ORDER = "D";
+  public static final String ORDER_CANCEL_REQUEST ="F";
+  public static final String ORDER_MASS_CANCEL_REQUEST = "q";
+  public static final String ORDER_CANCEL_REPLACE_REQUEST = "G";
+
+  /**
+   * Server-initiated message types as specified in section 7.1.2.2 of [2].
+   */
+  public static final String EXECUTION_REPORT = "8";
+  public static final String ORDER_CANCEL_REJECT ="9";
+  public static final String ORDER_MASS_CANCEL_REPORT = "r";
 
   private MessageType() {
   }
