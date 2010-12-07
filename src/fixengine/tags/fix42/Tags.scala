04f28ded1a890072d3be804f9f0d23fe8877e493
Karim Osman <karim.osman@reaktor.fi>
2010-12-07 17:24:29 +0200

    fixengine: Add Side(54)
    As of Scala 2.8.x, static forwarders are generated for fields (and
    methods) of objects. Unfortunately, fields in traits or super class are
    not taken into account, and thus, 'Tag' field must be introduced in each
    tag.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/tags/fix42/Tags.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.tags.fix42
+
+import fixengine.messages.{MessageTag, Value}
+import java.lang.Character
+
+object Side extends MessageTag[Character](54) {
+  val Tag = this
+  val Buy = Value('1')
+  val Sell = Value('2')
+  val BuyMinus = Value('3')
+  val SellPlus = Value('4')
+  val SellShort = Value('5')
+  val SellShortExempt = Value('6')
+  val Undisclosed = Value('7')
+  val Cross = Value('8')
+  val CrossShort = Value('9')
+  val CrossShortExempt = Value('A')
+  val AsDefined = Value('B')
+  val Opposite = Value('C')
+}
