d5a6b5ad10d0165cc65bed2684adfd9d24fe1b87
Karim Osman <karim.osman@reaktor.fi>
2010-12-07 17:24:28 +0200

    fixengine: Add MessageTag
    As fixengine.messages.Tag is already defined in Java, it cannot be used.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/MessageTag.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages
+
+import java.lang.Character
+
+abstract class MessageTag[T](value: Int) extends Tag[EnumField[Value[T]]](value, classOf[EnumField[Value[T]]]) {
+  def parse(v: T) = values.find(_.v == v).getOrElse(throw new InvalidValueForTagException(v.toString))
+  private def values: Array[Value[T]] = {
+    getClass.getDeclaredFields.filter { field =>
+      classOf[Value[T]].equals(field.getType)
+    }.map{ field =>
+      method(field).invoke(this).asInstanceOf[Value[T]]
+    }
+  }
+  private def method(field: java.lang.reflect.Field) = getClass.getDeclaredMethod(field.getName)
+}
+
+case class Value[T](val v: T) extends Formattable {
+  def value = v.toString
+}
---
+++ test/fixengine/messages/MessageTagSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages
+
+import org.specs.Specification
+import org.specs.runner.JUnit4
+
+import java.lang.Integer
+
+class MessageTagSpec extends JUnit4(MessageTagSpec)
+object MessageTagSpec extends Specification {
+  "MessageTag" should {
+    "resolve fields" in {
+      IntTag.parse(1) must equalTo(IntTag.Foo)
+      IntTag.parse(2) must equalTo(IntTag.Bar)
+    }
+  }
+  "MessageTag" should {
+    "throw an exception" in {
+      IntTag.parse(0) must throwAn[InvalidValueForTagException]
+    }
+  }
+  private object IntTag extends MessageTag[Integer](0) {
+    val Tag = this
+    val Foo = Value(1)
+    val Bar = Value(2)
+  }
+}
