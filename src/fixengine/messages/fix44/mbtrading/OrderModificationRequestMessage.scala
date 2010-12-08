2a952e80225fa8a0785d20d9751321240be20588
Karim Osman <karim.osman@reaktor.fi>
2010-12-08 16:17:24 +0200

    fixengine: Convert MB Trading specific Order Modification Request to Scala
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/mbtrading/OrderModificationRequestMessage.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44.mbtrading
+
+import fixengine.messages.AbstractMessage
+import fixengine.messages.MessageHeader
+import fixengine.messages.MessageVisitor
+import fixengine.messages.{OrderModificationRequestMessage => OrderModificationRequestMessageTrait}
+import fixengine.tags.Account
+import fixengine.tags.ClOrdID
+import fixengine.tags.HandlInst
+import fixengine.tags.OrdType
+import fixengine.tags.OrderQty
+import fixengine.tags.OrigClOrdID
+import fixengine.tags.Price
+import fixengine.tags.Side
+import fixengine.tags.Symbol
+import fixengine.tags.TimeInForce
+import fixengine.tags.TransactTime
+import fixengine.tags.fix44.Username
+
+class OrderModificationRequestMessage(header: MessageHeader) extends AbstractMessage(header) with OrderModificationRequestMessageTrait {
+  field(Account.TAG)
+  field(ClOrdID.TAG)
+  field(HandlInst.TAG)
+  field(OrderQty.TAG)
+  field(OrdType.TAG)
+  field(OrigClOrdID.TAG)
+  field(Price.TAG)
+  field(Side.TAG)
+  field(Symbol.TAG)
+  field(TimeInForce.TAG)
+  field(TransactTime.TAG)
+  field(Username.TAG)
+  override def apply(visitor: MessageVisitor) = visitor.visit(this)
+}
