bf00aa1ab4a53eb8f922eda7d1d392714527dcf1
Karim Osman <karim.osman@reaktor.fi>
2010-12-08 16:17:21 +0200

    fixengine: Convert New Order MultiLeg interface to Scala trait
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/NewOrderMultiLegMessage.scala
+++ src/fixengine/messages/NewOrderMultiLegMessage.scala
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fixengine.messages;
+package fixengine.messages
 
-public interface NewOrderMultiLegMessage extends RequestMessage {
-
-}
+trait NewOrderMultiLegMessage extends RequestMessage
