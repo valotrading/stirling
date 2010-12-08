b68535c1882e6d1f1f62ebc93a1548ad16db014b
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2010-12-08 15:38:28 +0200

    fixengine: Add Burgundy specific OrderCancelRejectMessage
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/burgundy/OrderCancelRejectMessage.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44.burgundy
+
+import fixengine.messages.AbstractMessage
+import fixengine.messages.MessageHeader
+import fixengine.messages.MessageVisitor
+import fixengine.messages.Required
+import fixengine.tags.Account
+import fixengine.tags.ClOrdID
+import fixengine.tags.CxlRejReason
+import fixengine.tags.CxlRejResponseTo
+import fixengine.tags.OrdStatus
+import fixengine.tags.OrderID
+import fixengine.tags.OrigClOrdID
+import fixengine.tags.Text
+import fixengine.tags.TransactTime
+import fixengine.messages.{OrderCancelRejectMessage => OrderCancelRejectMessageTrait}
+
+class OrderCancelRejectMessage(header: MessageHeader) extends AbstractMessage(header) with OrderCancelRejectMessageTrait {
+  field(Account.TAG, Required.NO)
+  field(ClOrdID.TAG)
+  field(OrderID.TAG, Required.NO)
+  field(OrdStatus.TAG)
+  field(OrigClOrdID.TAG)
+  field(TransactTime.TAG, Required.NO)
+  field(CxlRejResponseTo.TAG)
+  field(CxlRejReason.TAG, Required.NO)
+  field(Text.TAG, Required.NO)
+
+  override def apply(visitor: MessageVisitor) = visitor.visit(this)
+}
