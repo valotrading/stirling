f6f2b0892be732e3a10ad1ab4db89d7dea85e0c5
Karim Osman <karim.osman@reaktor.fi>
2010-12-08 16:17:19 +0200

    fixengine: Convert CollateralInquiryMessage interface to Scala trait
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/CollateralInquiryMessage.scala
+++ src/fixengine/messages/CollateralInquiryMessage.scala
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fixengine.messages;
+package fixengine.messages
 
-public interface CollateralInquiryMessage extends Message {
-}
+trait CollateralInquiryMessage extends Message
