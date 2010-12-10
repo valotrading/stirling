9bcf29248276be784d0ada3e27fff90ba007ddb8
Karim Osman <karim.osman@reaktor.fi>
2010-12-10 17:48:28 +0200

    fixengine: Support tags of type EnumTag in EnumField#parse
    Once all enumeration tags are defined using EnumTag, the type of
    EnumField is changed to Value<T> allowing removal of "instance of
    EnumTag" checks.
    
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/EnumField.java
+++ src/fixengine/messages/EnumField.java
@@ -15,10 +15,26 @@
  */
 package fixengine.messages;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author Pekka Enberg 
  */
-public abstract class EnumField<T extends Formattable> extends AbstractField<T> {
+public class EnumField<T extends Formattable> extends AbstractField<T> {
+    public EnumField(Tag<? extends EnumField<T>> tag) {
+        this(tag, Required.YES);
+    }
+
+    public EnumField(Tag<? extends EnumField<T>> tag, T value) {
+        this(tag, value, Required.YES);
+    }
+
+    public EnumField(Tag<? extends EnumField<T>> tag, T value, Required required) {
+        super(tag, value, required);
+    }
+
     public EnumField(Tag<? extends EnumField<T>> tag, Required required) {
         super(tag, null, required);
     }
@@ -27,10 +43,48 @@ public abstract class EnumField<T extends Formattable> extends AbstractField<T> 
         this.value = value;
     }
 
+    @Override public void parse(String value) {
+        if (tag() instanceof EnumTag) {
+            for (Field field : tagClass().getDeclaredFields()) {
+                if (!field.getType().equals(Value.class))
+                    continue;
+                T candidate = enumValue(field);
+                if (candidate.value().toString().equals(value)) {
+                    this.value = candidate;
+                    return;
+                }
+            }
+            throw new InvalidValueForTagException(value);
+        }
+        throw new UnsupportedOperationException("parsing not implemented for this tag type");
+    }
+
     @Override protected String value() {
         if (value == null) {
             return null;
         }
         return value.value();
+    }
+
+    @SuppressWarnings("unchecked") private T enumValue(Field field) {
+        try {
+            return (T) method(field).invoke(tag());
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Method method(Field field) {
+        try {
+            return tagClass().getDeclaredMethod(field.getName());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Class<? extends Tag> tagClass() {
+        return tag().getClass();
     }
 }
