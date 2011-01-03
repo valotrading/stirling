c36da353b4c7fe7337cd7656783ed71ea3dcac4a
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2011-01-03 13:21:51 +0200

    fixengine: Add Burgundy Dont Know Trade message
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/burgundy/DontKnowTrade.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44.burgundy
+
+import fixengine.messages.{
+AbstractMessage,
+MessageHeader,
+MessageVisitor,
+DontKnowTrade => DontKnowTradeTrait,
+Required
+}
+import fixengine.tags.fix43.SecurityIDSource
+import fixengine.tags.fix42.{DKReason, Text, SecurityID, Side, Symbol, OrderQty, OrderID, LastShares, LastPx, ExecID}
+
+class DontKnowTrade(header: MessageHeader) extends AbstractMessage(header) with DontKnowTradeTrait {
+  field(ExecID.Tag)
+  field(LastPx.Tag, Required.NO)
+  field(LastShares.Tag, Required.NO)
+  field(OrderID.Tag)
+  field(OrderQty.Tag)
+  field(Side.Tag)
+  field(Symbol.Tag)
+  field(SecurityIDSource.Tag, Required.NO)
+  field(SecurityID.Tag, Required.NO)
+  field(Text.Tag, Required.NO)
+  field(DKReason.Tag)
+
+  override def apply(visitor: MessageVisitor) = visitor.visit(this)
+}
