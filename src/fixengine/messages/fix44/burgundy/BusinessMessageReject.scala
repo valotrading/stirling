efc9e47be614285ad6025a584207d737bbe8cad4
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2011-01-03 13:21:49 +0200

    fixengine: Add Burgundy Business Message Reject message
    Signed-off-by: Heikki Rauhala <heikki.rauhala@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix44/burgundy/BusinessMessageReject.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix44.burgundy
+
+import fixengine.messages.{
+  AbstractMessage,
+  MessageHeader,
+  MessageVisitor,
+  BusinessMessageReject => BusinessMessageRejectTrait,
+  Required
+}
+import fixengine.tags.fix42.{
+  BusinessRejectRefID,
+  RefMsgType,
+  RefSeqNo,
+  Text
+}
+import fixengine.tags.fix43.BusinessRejectReason
+
+class BusinessMessageReject(header: MessageHeader) extends AbstractMessage(header) with BusinessMessageRejectTrait {
+  field(RefSeqNo.Tag)
+  field(Text.Tag, Required.NO)
+  field(RefMsgType.Tag)
+  field(BusinessRejectRefID.Tag, Required.NO)
+  field(BusinessRejectReason.Tag)
+
+  override def apply(visitor: MessageVisitor) = visitor.visit(this)
+}
