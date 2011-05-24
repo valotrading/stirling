60003a559e16a26319a3f4f96bdf14cd749ef211
Kare Nuorteva <kare.nuorteva@reaktor.fi>
2011-05-24 13:04:22 +0300

    fixengine: Convert HotspotFX NewOrderSingleMessage to Scala
    Signed-off-by: Kare Nuorteva <kare.nuorteva@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ src/fixengine/messages/fix42/hotspotfx/Messages.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fixengine.messages.fix42.hotspotfx
+
+import fixengine.messages.{
+  MessageHeader,
+  Required
+}
+import fixengine.tags.fix42.{
+  Account,
+  ClOrdID,
+  Currency,
+  ExecInst,
+  HandlInst,
+  MaxShow,
+  MinQty,
+  OrdType,
+  OrderQty,
+  PegDifference,
+  Price,
+  Side,
+  Symbol,
+  TimeInForce,
+  TransactTime
+}
+import fixengine.tags.fix44.TradeLinkID
+
+class NewOrderSingleMessage(header: MessageHeader) extends fixengine.messages.fix42.NewOrderSingleMessage(header) {
+  field(Account.Tag, Required.NO)
+  field(ClOrdID.Tag)
+  field(ExecInst.Tag, Required.NO)
+  field(HandlInst.Tag)
+  field(Currency.Tag, Required.NO)
+  field(OrderQty.Tag)
+  field(MaxShow.Tag, Required.NO)
+  field(MinQty.Tag, Required.NO)
+  field(Price.Tag)
+  field(Side.Tag)
+  field(Symbol.Tag)
+  field(OrdType.Tag)
+  field(TimeInForce.Tag, Required.NO)
+  field(TransactTime.Tag, Required.NO)
+  field(PegDifference.Tag, Required.NO)
+  field(TradeLinkID.Tag, Required.NO)
+}
