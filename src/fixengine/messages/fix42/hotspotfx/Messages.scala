f417e7fc5dc9cb349e99008dd2aa3f719f203f39
Kare Nuorteva <kare.nuorteva@reaktor.fi>
2011-05-24 13:04:25 +0300

    fixengine: Convert HotspotFX ExecutionReport to Scala
    Signed-off-by: Kare Nuorteva <kare.nuorteva@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/fix42/hotspotfx/Messages.scala
+++ src/fixengine/messages/fix42/hotspotfx/Messages.scala
@@ -21,20 +21,41 @@ import fixengine.messages.{
 }
 import fixengine.tags.fix42.{
   Account,
+  AvgPx,
   ClOrdID,
+  ContraBroker,
+  CumQty,
   Currency,
+  ExecBroker,
+  ExecID,
   ExecInst,
+  ExecTransType,
+  ExecType,
+  ExpireTime,
+  FutSettDate,
   HandlInst,
+  LastPx,
+  LastShares,
+  LeavesQty,
   MaxShow,
   MinQty,
+  NoContraBrokers,
+  OrdStatus,
   OrdType,
+  OrderID,
   OrderQty,
+  OrderQty2,
   OrigClOrdID,
   PegDifference,
   Price,
+  SecurityType,
+  SettlCurrAmt,
+  SettlCurrency,
   Side,
   Symbol,
+  Text,
   TimeInForce,
+  TradeDate,
   TransactTime
 }
 import fixengine.tags.fix44.TradeLinkID
@@ -81,4 +102,42 @@ class OrderModificationRequestMessage(header: MessageHeader) extends fixengine.m
   field(OrdType.Tag, Required.NO)
   field(Side.Tag, Required.NO)
   field(TransactTime.Tag, Required.NO)
+}
+
+class ExecutionReport(header: MessageHeader) extends fixengine.messages.fix42.ExecutionReport(header) {
+  field(Account.Tag, Required.NO)
+  field(ClOrdID.Tag)
+  field(ExecID.Tag)
+  field(ExecInst.Tag, Required.NO)
+  field(ExecTransType.Tag)
+  field(OrderID.Tag)
+  field(OrdStatus.Tag)
+  field(OrigClOrdID.Tag, Required.NO)
+  field(OrderQty.Tag, Required.NO)
+  field(Price.Tag, Required.NO)
+  field(TimeInForce.Tag, Required.NO)
+  field(Currency.Tag, Required.NO)
+  field(Side.Tag)
+  field(Symbol.Tag)
+  field(ExecBroker.Tag, Required.NO)
+  field(LeavesQty.Tag)
+  field(CumQty.Tag)
+  field(MinQty.Tag, Required.NO)
+  field(OrderQty2.Tag, Required.NO)
+  field(FutSettDate.Tag, Required.NO)
+  field(TradeDate.Tag, Required.NO)
+  field(SettlCurrAmt.Tag, Required.NO)
+  field(SettlCurrency.Tag, Required.NO)
+  field(SecurityType.Tag)
+  field(MaxShow.Tag, Required.NO)
+  field(ExecType.Tag)
+  field(AvgPx.Tag)
+  field(LastShares.Tag, Required.NO)
+  field(LastPx.Tag, Required.NO)
+  field(NoContraBrokers.Tag, Required.NO)
+  field(ContraBroker.Tag, Required.NO)
+  field(Text.Tag, Required.NO)
+  field(TransactTime.Tag)
+  field(ExpireTime.Tag, Required.NO)
+  field(TradeLinkID.Tag, Required.NO)
 }
