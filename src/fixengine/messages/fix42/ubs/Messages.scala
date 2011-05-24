e6346f9c03d8b9be5f6fb942ef203ac4ca0b8800
Kare Nuorteva <kare.nuorteva@reaktor.fi>
2011-05-24 16:55:14 +0300

    fixengine: Convert UBS ExecutionReport to Scala
    Signed-off-by: Kare Nuorteva <kare.nuorteva@reaktor.fi>
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/fix42/ubs/Messages.scala
+++ src/fixengine/messages/fix42/ubs/Messages.scala
@@ -18,14 +18,26 @@ package fixengine.messages.fix42.ubs
 import fixengine.messages.MessageHeader
 import fixengine.messages.Required
 import fixengine.tags.fix42.{
+  AvgPx,
+  ClOrdID,
+  CumQty,
   DKReason,
   ExecID,
+  ExecRefID,
+  ExecTransType,
+  ExecType,
+  LastCapacity,
+  LastMkt,
   LastPx,
   LastShares,
+  LeavesQty,
+  OrdStatus,
   OrderID,
   OrderQty,
+  OrigClOrdID,
   Side,
-  Symbol
+  Symbol,
+  TransactTime
 }
 
 class DontKnowTradeMessage(header: MessageHeader) extends fixengine.messages.fix42.DontKnowTradeMessage(header) {
@@ -37,4 +49,31 @@ class DontKnowTradeMessage(header: MessageHeader) extends fixengine.messages.fix
   field(OrderQty.Tag, Required.NO)
   field(LastShares.Tag, Required.NO)
   field(LastPx.Tag, Required.NO)
+}
+
+class ExecutionReport(header: MessageHeader) extends fixengine.messages.fix42.ExecutionReport(header) {
+  field(OrderID.Tag)
+  field(ClOrdID.Tag)
+  field(OrigClOrdID.Tag, Required.NO)
+  field(ExecID.Tag)
+  field(ExecType.Tag)
+  field(ExecTransType.Tag)
+  field(ExecRefID.Tag, new Required {
+    override def isRequired: Boolean = {
+      val value = getEnum(ExecTransType.Tag)
+      value.equals(ExecTransType.Cancel) || value.equals(ExecTransType.Correct)
+    }
+  })
+  field(Symbol.Tag)
+  field(Side.Tag)
+  field(OrderQty.Tag)
+  field(OrdStatus.Tag)
+  field(LastShares.Tag, Required.NO)
+  field(LastPx.Tag, Required.NO)
+  field(LastMkt.Tag, Required.NO)
+  field(LastCapacity.Tag, Required.NO)
+  field(LeavesQty.Tag)
+  field(CumQty.Tag)
+  field(AvgPx.Tag)
+  field(TransactTime.Tag)
 }
