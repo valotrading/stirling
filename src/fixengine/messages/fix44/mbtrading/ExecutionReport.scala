f2cd611aa69172ad758fc1bf80bf4406280ad70f
Karim Osman <karim.osman@reaktor.fi>
2011-07-11 23:02:55 +0300

    fixengine: Revise MB Trading specific Execution Report
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/messages/fix44/mbtrading/ExecutionReport.scala
+++ src/fixengine/messages/fix44/mbtrading/ExecutionReport.scala
@@ -5,13 +5,17 @@ import fixengine.messages.{
   ExecutionReport => ExecutionReportTrait,
   MessageHeader,
   MessageVisitor,
+  RepeatingGroup,
+  RepeatingGroupInstance,
   Required
 }
 import fixengine.tags.fix42.{
   Account,
   AvgPx,
   ClOrdID,
+  ClientID,
   Commission,
+  ComplianceID,
   CumQty,
   DiscretionInst,
   DiscretionOffset,
@@ -19,31 +23,39 @@ import fixengine.tags.fix42.{
   ExDestination,
   ExecID,
   ExecInst,
-  ExecType,
   ExpireTime,
   LastPx,
   LastShares,
   LeavesQty,
   MaxFloor,
+  OrdRejReason,
   OrdStatus,
   OrdType,
   OrderID,
   OrderQty,
   OrigClOrdID,
   Price,
-  Side,
   SecondaryOrderID,
+  SecurityType,
+  Side,
   StopPx,
   Symbol,
   Text,
   TimeInForce,
-  TransactTime
+  TransactTime,
+  UnderlyingSymbol
 }
 import fixengine.tags.fix43.{
   LegPrice,
+  LegProduct,
   LegRefID,
+  LegRatioQty,
+  LegStrikePrice,
+  LegSymbol,
+  LegSide,
   MassStatusReqID,
   MultiLegReportingType,
+  NoLegs,
   OrderRestrictions,
   PositionEffect,
   Price2,
@@ -56,6 +68,7 @@ import fixengine.tags.fix44.mbtrading.{
   PosRealizedPNL
 }
 import fixengine.tags.fix44.{
+  ExecType,
   LastRptRequested
 }
 class ExecutionReport(header: MessageHeader) extends AbstractMessage(header) with ExecutionReportTrait {
@@ -74,34 +87,48 @@ class ExecutionReport(header: MessageHeader) extends AbstractMessage(header) wit
   field(OrdType.Tag, Required.NO)
   field(OrigClOrdID.Tag, Required.NO)
   field(Price.Tag, Required.NO)
-  field(Symbol.Tag)
   field(Side.Tag)
+  field(Symbol.Tag)
   field(Text.Tag, Required.NO)
   field(TimeInForce.Tag, Required.NO)
   field(TransactTime.Tag, Required.NO)
   field(PositionEffect.Tag, Required.NO)
   field(StopPx.Tag, Required.NO)
   field(ExDestination.Tag, Required.NO)
+  field(OrdRejReason.Tag, Required.NO)
+  field(ClientID.Tag)
   field(MaxFloor.Tag, Required.NO)
   field(ExpireTime.Tag, Required.NO)
   field(ExecType.Tag)
   field(LeavesQty.Tag)
+  field(SecurityType.Tag, Required.NO)
   field(EffectiveTime.Tag, Required.NO)
   field(SecondaryOrderID.Tag, Required.NO)
+  field(UnderlyingSymbol.Tag, Required.NO)
+  field(ComplianceID.Tag, Required.NO)
   field(DiscretionInst.Tag, Required.NO)
   field(DiscretionOffset.Tag, Required.NO)
   field(MultiLegReportingType.Tag, Required.NO)
   field(Product.Tag, Required.NO)
   field(OrderRestrictions.Tag, Required.NO)
-  field(LegPrice.Tag, Required.NO)
   field(MassStatusReqID.Tag, Required.NO)
-  field(Price2.Tag, Required.NO)
-  field(LegRefID.Tag, Required.NO)
+  group(new RepeatingGroup(NoLegs.Tag) {
+    override def newInstance:RepeatingGroupInstance = {
+      return new RepeatingGroupInstance(LegPrice.Tag) {
+        field(LegSymbol.Tag, Required.NO)
+        field(LegProduct.Tag, Required.NO)
+        field(LegStrikePrice.Tag, Required.NO)
+        field(LegRatioQty.Tag, Required.NO)
+        field(LegSide.Tag, Required.NO)
+        field(LegRefID.Tag, Required.NO)
+      }
+    }
+  }, Required.NO)
   field(LastRptRequested.Tag, Required.NO)
   field(LiquidityTag.Tag, Required.NO)
   field(PosRealizedPNL.Tag, Required.NO)
-  field(MBTInternalOrderId.Tag, Required.NO)
   field(OrderGroupID1.Tag, Required.NO)
-
+  field(Price2.Tag, Required.NO)
+  field(MBTInternalOrderId.Tag, Required.NO)
   override def apply(visitor: MessageVisitor) = visitor.visit(this)
 }
