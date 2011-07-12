ce8e4df25fa443b09885b680cab510c4b99b0942
Karim Osman <karim.osman@reaktor.fi>
2011-07-12 19:13:40 +0300

    fixengine: Revise MB Trading specific tags
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- src/fixengine/tags/fix44/mbtrading/Tags.scala
+++ src/fixengine/tags/fix44/mbtrading/Tags.scala
@@ -16,52 +16,40 @@
 package fixengine.tags.fix44.mbtrading
 
 import fixengine.messages.{
+  EnumTag,
   FloatTag,
   IntegerTag,
-  EnumTag,
+  IntegerValue,
   QtyTag,
   StringTag
 }
 import java.lang.Integer
 
 object PosPendBuy extends QtyTag(10000)
-
 object PosPendSell extends QtyTag(10001)
-
 object MorningBuyingPower extends StringTag(10002)
-
 object PosBuyPowerUsed extends FloatTag(10003)
-
 object PosRealizedPNL extends FloatTag(10004)
-
 object MBTAccountType extends EnumTag[Integer](10005) {
-  val Cash = 1
-  val USMArgin = 2
-  val DVP = 6
+  val Cash = IntegerValue(1)
+  val USMArgin = IntegerValue(2)
+  val DVP = IntegerValue(6)
+  val Forex = IntegerValue(20)
 }
-
 object OvernightBuyingPower extends StringTag(10006)
-
 object RealizedPnL extends StringTag(10008)
-
-object MorningExcessEquity extends StringTag(10009)
-
-object MorningExcessEquity2 extends StringTag(10010)
-
+object MorningAccountValue extends StringTag(10009)
+object MorningExcessEquity extends StringTag(10010)
 object PosEquityUsed extends FloatTag(10015)
-
 object AccountCredit extends StringTag(10018)
-
 object MBTXAggressive extends IntegerTag(10022)
-
+object MorningExcessEquity2 extends StringTag(10040)
 object OvernightExcess extends StringTag(10048)
-
 object BODOOvernightExcessEq extends StringTag(10050)
-
 object OrderGroupID1 extends StringTag(10055)
-
 object LiquidityTag extends StringTag(9730)
-
 object MBTInternalOrderId extends StringTag(10017)
-
 object UserQuotePerms extends StringTag(10059)
+object FLID extends StringTag(10401)
+object UserSessionID extends StringTag(10401)
+object UnknownMBTradingTag1 extends StringTag(10024)
