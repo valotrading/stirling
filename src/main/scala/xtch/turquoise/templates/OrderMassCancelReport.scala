3b50f15b079fcf3a0a9decac3bc4ba920f83857f
Jussi Virtanen <jussi.virtanen@reaktor.fi>
2011-10-27 14:53:36 +0300

    xtch: Convert OrderMassCancelReport to Scala
    While at it, fix the message type.
    
    Signed-off-by: Jussi Virtanen <jussi.virtanen@reaktor.fi>

--- src/main/scala/xtch/turquoise/templates/OrderMassCancelReport.scala
+++ src/main/scala/xtch/turquoise/templates/OrderMassCancelReport.scala
@@ -13,27 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package xtch.turquoise.templates;
+package xtch.turquoise.templates
 
-import xtch.turquoise.Fields;
-import xtch.turquoise.MessageType;
+import xtch.turquoise.Fields
+import xtch.turquoise.MessageType
 
-/** 
+/**
  * Template for OrderMassCancelReport message as specified in section 7.4.7 of [2].
  */
-public class OrderMassCancelReport extends AbstractTemplate {
-  public static final OrderMassCancelReport TEMPLATE = new OrderMassCancelReport();
-
-  private OrderMassCancelReport() {
-    super(MessageType.LOGOUT);
-    add(MessageHeader.TEMPLATE);
-    add(Fields.APP_ID);
-    add(Fields.SEQUENCE_NO);
-    add(Fields.CLIENT_ORDER_ID);
-    add(Fields.MASS_CANCEL_RESPONSE);
-    add(Fields.MASS_CANCEL_REJECT_REASON);
-    add(Fields.RESERVED_FIELD_1);
-    add(Fields.TRANSACT_TIME);
-    add(Fields.RESERVED_FIELD_2);
-  }
+object OrderMassCancelReport extends AbstractTemplate(MessageType.ORDER_MASS_CANCEL_REPORT) {
+  add(MessageHeader.TEMPLATE);
+  add(Fields.APP_ID);
+  add(Fields.SEQUENCE_NO);
+  add(Fields.CLIENT_ORDER_ID);
+  add(Fields.MASS_CANCEL_RESPONSE);
+  add(Fields.MASS_CANCEL_REJECT_REASON);
+  add(Fields.RESERVED_FIELD_1);
+  add(Fields.TRANSACT_TIME);
+  add(Fields.RESERVED_FIELD_2);
 }
