4270980c9ab75c1670e7ab86db24a5acafacb171
Jussi Virtanen <jussi.virtanen@reaktor.fi>
2011-10-27 14:11:36 +0300

    xtch: Convert Logon to Scala
    Signed-off-by: Jussi Virtanen <jussi.virtanen@reaktor.fi>

--- src/main/scala/xtch/turquoise/templates/Logon.scala
+++ src/main/scala/xtch/turquoise/templates/Logon.scala
@@ -13,23 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package xtch.turquoise.templates;
+package xtch.turquoise.templates
 
-import xtch.turquoise.Fields;
-import xtch.turquoise.MessageType;
+import xtch.turquoise.Fields
+import xtch.turquoise.MessageType
 
-/** 
+/**
  * Template for Logon message as specified in section 7.3.1 of [2].
  */
-public class Logon extends AbstractTemplate {
-  public static final Logon TEMPLATE = new Logon();
-
-  private Logon() {
-    super(MessageType.LOGON);
-    add(MessageHeader.TEMPLATE);
-    add(Fields.COMP_ID);
-    add(Fields.PASSWORD);
-    add(Fields.NEW_PASSWORD);
-    add(Fields.MESSAGE_VERSION);
-  }
+object Logon extends AbstractTemplate(MessageType.LOGON) {
+  add(MessageHeader.TEMPLATE);
+  add(Fields.COMP_ID);
+  add(Fields.PASSWORD);
+  add(Fields.NEW_PASSWORD);
+  add(Fields.MESSAGE_VERSION);
 }
