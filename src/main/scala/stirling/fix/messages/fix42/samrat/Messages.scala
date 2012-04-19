73439d7a39de4bc9abb8facb1f2a4c8dc80837fc
Jussi Virtanen <jussi.virtanen@valotrading.com>
2012-04-19 12:37:18 +0300

    fix: Add Samrat specific NewOrderSingle
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

--- src/main/scala/stirling/fix/messages/fix42/samrat/Messages.scala
+++ src/main/scala/stirling/fix/messages/fix42/samrat/Messages.scala
@@ -20,21 +20,49 @@ import stirling.fix.messages.{
   Logon => LogonTrait,
   MessageHeader,
   MessageVisitor,
+  NewOrderSingle => NewOrderSingleTrait,
   Reject => RejectTrait,
   Required
 }
 import stirling.fix.tags.fix42.{
+  ClOrdID,
+  DiscretionOffset,
   EncryptMethod,
+  ExpireTime,
   HeartBtInt,
+  MaxFloor,
+  MinQty,
+  OrdType,
+  OrderQty,
+  PegDifference,
+  Price,
   RefMsgType,
   RefSeqNo,
   RefTagId,
+  SecurityID,
+  Symbol,
+  SymbolSfx,
   Text
 }
 import stirling.fix.tags.fix42.samrat.{
-  CancelAllOnDisconnect
+  AllowRouting,
+  AlternateExDestination,
+  CancelAllOnDisconnect,
+  ISO,
+  Invisible,
+  LockedOrCrossedAction,
+  LongSaleAffirm,
+  PegType,
+  PostOnly,
+  RegularSessionOnly,
+  RouteToNYSE,
+  ShortSaleAffirm,
+  ShortSaleAffirmLongQuantity,
+  Side,
+  TimeInForce
 }
 import stirling.fix.tags.fix43.{
+  SecurityIDSource,
   SessionRejectReason
 }
 import stirling.fix.tags.fix44.{
@@ -57,5 +85,36 @@ class Reject(header: MessageHeader) extends AbstractMessage(header) with RejectT
   field(RefTagId.Tag, Required.NO)
   field(RefMsgType.Tag, Required.NO)
   field(SessionRejectReason.Tag, Required.NO)
+  def apply(visitor: MessageVisitor) = visitor.visit(this)
+}
+
+class NewOrderSingle(header: MessageHeader) extends AbstractMessage(header) with NewOrderSingleTrait {
+  field(ClOrdID.Tag)
+  field(Symbol.Tag)
+  field(SymbolSfx.Tag, Required.NO)
+  field(SecurityID.Tag, Required.NO)
+  field(SecurityIDSource.Tag, Required.NO)
+  field(Side.Tag)
+  field(OrderQty.Tag)
+  field(OrdType.Tag)
+  field(Price.Tag, Required.NO)
+  field(TimeInForce.Tag, Required.NO)
+  field(MinQty.Tag, Required.NO)
+  field(MaxFloor.Tag, Required.NO)
+  field(ExpireTime.Tag, Required.NO)
+  field(PegDifference.Tag, Required.NO)
+  field(DiscretionOffset.Tag, Required.NO)
+  field(Invisible.Tag, Required.NO)
+  field(PostOnly.Tag, Required.NO)
+  field(ShortSaleAffirm.Tag, Required.NO)
+  field(ShortSaleAffirmLongQuantity.Tag, Required.NO)
+  field(LongSaleAffirm.Tag, Required.NO)
+  field(AllowRouting.Tag, Required.NO)
+  field(AlternateExDestination.Tag, Required.NO)
+  field(RouteToNYSE.Tag, Required.NO)
+  field(ISO.Tag, Required.NO)
+  field(PegType.Tag, Required.NO)
+  field(LockedOrCrossedAction.Tag, Required.NO)
+  field(RegularSessionOnly.Tag, Required.NO)
   def apply(visitor: MessageVisitor) = visitor.visit(this)
 }
