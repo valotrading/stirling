8052a52f21b170b20c0c828bf17f629f43e38de9
Jussi Virtanen <jussi.virtanen@valotrading.com>
2012-04-20 14:23:41 +0300

    fix: Add Samrat specific MessageFactory
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

---
+++ src/main/scala/stirling/fix/messages/fix42/samrat/MessageFactory.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package stirling.fix.messages.fix42.samrat
+
+import stirling.fix.messages.fix42.DefaultMessageFactory
+import stirling.fix.messages.fix42.MsgTypeValue._
+import stirling.fix.messages.fix42.samrat.MsgTypeValue._
+
+class MessageFactory extends DefaultMessageFactory {
+  message(LOGON, classOf[Logon])
+  message(REJECT, classOf[Reject])
+  message(NEW_ORDER_SINGLE, classOf[NewOrderSingle])
+  message(ORDER_CANCEL_REQUEST, classOf[OrderCancelRequest])
+  message(ORDER_CANCEL_REPLACE_REQUEST, classOf[OrderCancelReplaceRequest])
+  message(BULK_CANCEL_REQUEST, classOf[BulkCancelRequest])
+  message(EXECUTION_REPORT, classOf[ExecutionReport])
+  override def getProfile = "samrat"
+  override def isValid(msgType: String) = {
+    if (msgType.equals(BULK_CANCEL_REQUEST))
+      true
+    else
+      super.isValid(msgType)
+  }
+}
