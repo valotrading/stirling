a9de3f308b3a0355df2ec44fc675de902c8649e3
Jussi Virtanen <jussi.virtanen@valotrading.com>
2012-05-02 15:20:47 +0300

    fix: Extract AbstractMessageHeader
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

--- src/main/java/stirling/fix/messages/DefaultMessageHeader.java
+++ src/main/java/stirling/fix/messages/DefaultMessageHeader.java
@@ -15,17 +15,9 @@
  */
 package stirling.fix.messages;
 
-import java.nio.ByteBuffer;
-
-import org.joda.time.DateTime;
-import org.joda.time.Minutes;
-
-import stirling.fix.tags.fix42.BeginString;
-import stirling.fix.tags.fix42.BodyLength;
 import stirling.fix.tags.fix42.DeliverToCompID;
 import stirling.fix.tags.fix42.DeliverToSubID;
 import stirling.fix.tags.fix42.MsgSeqNum;
-import stirling.fix.tags.fix42.MsgType;
 import stirling.fix.tags.fix42.OnBehalfOfCompID;
 import stirling.fix.tags.fix42.OnBehalfOfSubID;
 import stirling.fix.tags.fix42.OrigSendingTime;
@@ -41,10 +33,7 @@ import stirling.fix.tags.fix42.SenderLocationID;
 /**
  * @author Pekka Enberg 
  */
-public class DefaultMessageHeader extends FieldContainer implements Formattable, Parseable {
-    private static final Minutes MAX_TIME_DIFFERENCE = Minutes.TWO;
-
-    private final FieldContainer head = new FieldContainer();
+public class DefaultMessageHeader extends AbstractMessageHeader {
 
     public DefaultMessageHeader(String msgType) {
         this();
@@ -53,9 +42,6 @@ public class DefaultMessageHeader extends FieldContainer implements Formattable,
     }
 
     public DefaultMessageHeader() {
-        head.field(BeginString.Tag());
-        head.field(BodyLength.Tag());
-        head.field(MsgType.Tag());
         field(SenderCompID.Tag());
         field(SenderSubID.Tag(), Required.NO);
         field(TargetCompID.Tag());
@@ -74,63 +60,5 @@ public class DefaultMessageHeader extends FieldContainer implements Formattable,
             }
         });
         field(SenderLocationID.Tag(), Required.NO);
-    }
-
-    @Override public void parse(ByteBuffer b) {
-        parseHeadField(b, BeginString.Tag());
-        parseHeadField(b, BodyLength.Tag());
-        trailer(b);
-        parseHeadField(b, MsgType.Tag());
-        super.parse(b);
-    }
-
-    private void trailer(ByteBuffer b) {
-        int checkSumPosition = b.position() + getBodyLength();
-        b.limit(checkSumPosition);
-    }
-
-    private void parseHeadField(ByteBuffer b, Tag<?> tag) {
-        Tag.parseTag(b);
-        Field field = head.lookup(tag.value());
-        field.parse(b);
-    }
-
-    public String getBeginString() {
-        return head.getString(BeginString.Tag());
-    }
-
-    public void setBeginString(String beginString) {
-        head.setString(BeginString.Tag(), beginString);
-    }
-
-    public int getBodyLength() {
-        return head.getInteger(BodyLength.Tag());
-    }
-
-    public String getMsgType() {
-        return head.getString(MsgType.Tag());
-    }
-
-    public void setMsgType(String msgType) {
-        head.setString(MsgType.Tag(), msgType);
-    }
-
-    public boolean isPointToPoint() {
-        return !hasValue(OnBehalfOfCompID.Tag()) && !hasValue(DeliverToCompID.Tag());
-    }
-
-    public boolean hasAccurateSendingTime(DateTime currentTime) {
-        if (!hasValue(SendingTime.Tag())) {
-            return true;
-        }
-        Minutes difference = Minutes.minutesBetween(currentTime, getDateTime(SendingTime.Tag()));
-        return Math.abs(difference.getMinutes()) < MAX_TIME_DIFFERENCE.getMinutes();
-    }
-
-    public boolean hasOrigSendingTimeEarlierThanOrEqualToSendingTime() {
-        if (!getBoolean(PossDupFlag.Tag()) || !hasValue(OrigSendingTime.Tag())) {
-            return true;
-        }
-        return !getDateTime(OrigSendingTime.Tag()).isAfter(getDateTime(SendingTime.Tag()));
     }
 }
