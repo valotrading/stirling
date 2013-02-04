2766ca64602513eb02f81985a27d828d74cc4eb1
Jussi Virtanen <jussi.virtanen@valotrading.com>
2013-02-04 13:51:17 +0200

    Rename 'stirling.itch.nasdaq' to 'stirling.nasdaq'
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

--- src/main/scala/stirling/nasdaq/itch41/Enumerations.scala
+++ src/main/scala/stirling/nasdaq/itch41/Enumerations.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package stirling.itch.nasdaq.itch41
+package stirling.nasdaq.itch41
 
 /*
  * Section 4.2
