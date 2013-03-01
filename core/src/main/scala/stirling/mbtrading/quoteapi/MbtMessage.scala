c50a9a0bf16c9a6a537ec88699128c5219d2e2e5
Jussi Virtanen <jussi.virtanen@valotrading.com>
2013-03-01 16:41:02 +0200

    Add some more whitespace to MBT Quote API
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

--- core/src/main/scala/stirling/mbtrading/quoteapi/MbtMessage.scala
+++ core/src/main/scala/stirling/mbtrading/quoteapi/MbtMessage.scala
@@ -21,8 +21,8 @@ import org.joda.time.format.DateTimeFormat
 import silvertip.PartialMessageException
 
 object MbtMessage {
-  type Tag = Int
-  type Value = String
+  type Tag         = Int
+  type Value       = String
   type MessageType = Char
 
   def parse(buffer: ByteBuffer): MbtMessage = {
@@ -135,109 +135,110 @@ case class MbtMessage(msgType: MessageType, fields: Map[MbtMessage.Tag, MbtMessa
 }
 
 object MbtMessageType {
-  val Login = 'L'
-  val Subscription = 'S'
-  val Unsubscription = 'U'
-  val Fundamental = 'H'
-  val Heartbeat = '9'
-  val LogonAccepted = 'G'
-  val LogonDenied = 'D'
-  val Level1Update = '1'
-  val Level2Update = '2'
-  val TasUpdate = '3'
+  val Login                   = 'L'
+  val Subscription            = 'S'
+  val Unsubscription          = 'U'
+  val Fundamental             = 'H'
+  val Heartbeat               = '9'
+  val LogonAccepted           = 'G'
+  val LogonDenied             = 'D'
+  val Level1Update            = '1'
+  val Level2Update            = '2'
+  val TasUpdate               = '3'
   val FundamentalDataResponse = 'N'
-  val OptionsChainsUpdate = '4'
+  val OptionsChainsUpdate     = '4'
+
   val marketDataMessages = Set(Level1Update, Level2Update, TasUpdate, FundamentalDataResponse, OptionsChainsUpdate)
 }
 
 object MbtMessageTag {
-  val CompanyName = 2021
-  val ContractSize = 2041
-  val Country = 2025
-  val Currency = 2026
-  val CurrentTick = 2013
-  val Cusip = 2028
-  val Date = 2015
-  val Exchange = 2022
-  val ExpMonth = 2036
-  val ExpYear = 2040
-  val High = 2009
-  val InfoMsgFrom = 8055
-  val Isin = 2029
-  val LastAsk = 2004
-  val LastAskSize = 2006
-  val LastBid = 2003
-  val LastBidSize = 2005
-  val LastExchange = 2042
-  val Size = 2007
-  val LoginReason = 103
-  val Low = 2010
+  val CompanyName      = 2021
+  val ContractSize     = 2041
+  val Country          = 2025
+  val Currency         = 2026
+  val CurrentTick      = 2013
+  val Cusip            = 2028
+  val Date             = 2015
+  val Exchange         = 2022
+  val ExpMonth         = 2036
+  val ExpYear          = 2040
+  val High             = 2009
+  val InfoMsgFrom      = 8055
+  val Isin             = 2029
+  val LastAsk          = 2004
+  val LastAskSize      = 2006
+  val LastBid          = 2003
+  val LastBidSize      = 2005
+  val LastExchange     = 2042
+  val Size             = 2007
+  val LoginReason      = 103
+  val Low              = 2010
   val MarginMultiplier = 2031
-  val MmidPrice = 2019
-  val MmidSource = 2033
-  val MmidStatus = 2024
-  val MmidTime = 2020
-  val Mpid = 2016
-  val Open = 2011
-  val OpenInterest = 2037
-  val Password = 101
-  val PrevClose = 2008
-  val Price = 2002
-  val PutOrCall = 2038
-  val QuotePrice = 2018
-  val QuoteSize = 2017
-  val RequestRejected = 1011
-  val StrikePrice = 2035
+  val MmidPrice        = 2019
+  val MmidSource       = 2033
+  val MmidStatus       = 2024
+  val MmidTime         = 2020
+  val Mpid             = 2016
+  val Open             = 2011
+  val OpenInterest     = 2037
+  val Password         = 101
+  val PrevClose        = 2008
+  val Price            = 2002
+  val PutOrCall        = 2038
+  val QuotePrice       = 2018
+  val QuoteSize        = 2017
+  val RequestRejected  = 1011
+  val StrikePrice      = 2035
   val SubscriptionType = 2000
-  val Symbol = 1003
-  val TickCondition = 2082
-  val TickSize = 2027
-  val TickStatus = 2083
-  val TickType = 2084
-  val Timestamp = 2014
-  val TotalVolume = 2012
-  val TradingStatus = 2032
-  val UpcInfo = 2023
-  val TasType = 2039
-  val Username = 100
-  val UserPermissions = 154
+  val Symbol           = 1003
+  val TickCondition    = 2082
+  val TickSize         = 2027
+  val TickStatus       = 2083
+  val TickType         = 2084
+  val Timestamp        = 2014
+  val TotalVolume      = 2012
+  val TradingStatus    = 2032
+  val UpcInfo          = 2023
+  val TasType          = 2039
+  val Username         = 100
+  val UserPermissions  = 154
 }
 
 object MbtQuoteServerTick {
-  val Uptick = 20020
+  val Uptick   = 20020
   val Downtick = 20021
 }
 
 object MbtQuoteServerSubscriptionType {
-  val Level1 = 20000
-  val Level2 = 20001
+  val Level1          = 20000
+  val Level2          = 20001
   val Level1AndLevel2 = 20002
-  val Trades = 20003
-  val OptionsChains = 20004
+  val Trades          = 20003
+  val OptionsChains   = 20004
 }
 
 object MbtQuoteServerTasType {
   val Normal = 30030
-  val FormT = 30031
+  val FormT  = 30031
 }
 
 object MbtQuoteServerTickCondition {
-  val RegularSale = 0
-  val FormT = 29
+  val RegularSale      = 0
+  val FormT            = 29
   val IntermarketSweep = 45
-  val ClosePrice = 49
-  val AveragePrice = 53
+  val ClosePrice       = 49
+  val AveragePrice     = 53
 }
 
 object MbtQuoteServerTickStatus {
-  val Normal = 0
-  val Filtered = 1
+  val Normal        = 0
+  val Filtered      = 1
   val OutOfSequence = 2
-  val Deleted = 3
+  val Deleted       = 3
 }
 
 object MbtQuoteServerTickType {
   val Trade = 0
-  val Bid = 1
-  val Ask = 2
+  val Bid   = 1
+  val Ask   = 2
 }
