3de207960aded41a11bd6d12eeee80e8fe0c894d
Jussi Virtanen <jussi.virtanen@valotrading.com>
2013-03-21 15:38:26 +0200

    Add BATS PITCH 1.12.0 specific constants
    Signed-off-by: Jussi Virtanen <jussi.virtanen@valotrading.com>

---
+++ core/src/main/scala/stirling/bats/pitch1120/package.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package stirling.bats
+
+package object pitch1120 {
+
+  /*
+   * Section 4.3
+   */
+  val SideIndicator = pitch1122.SideIndicator
+
+  /*
+   * Section 4.7
+   */
+  val HaltStatus = pitch1122.HaltStatus
+
+  /*
+   * Section 4.7
+   */
+  val RegSHOAction = pitch1122.RegSHOAction
+
+  /*
+   * Section 4.8
+   */
+  val AuctionType = pitch1122.AuctionType
+
+  /*
+   * Section 4.10
+   */
+  val RPI = pitch1122.RPI
+}
